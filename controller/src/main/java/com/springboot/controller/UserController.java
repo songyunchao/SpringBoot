bf9660f56eb8d523bcd1cfee486359a3c44d9219
fsun <385207720@qq.com>
2019-05-16 10:26:47 +0800

    122131

--- controller/src/main/java/com/springboot/controller/UserController.java
+++ controller/src/main/java/com/springboot/controller/UserController.java
@@ -4,9 +4,8 @@ import com.spring.domain.SysUser;
 import com.springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("User")
@@ -15,8 +14,8 @@ public class UserController
     @Autowired
     private UserService userService;
         @RequestMapping("/getUserById")
-        public  String getUserByPrimaryKey(String id){
-           SysUser user = userService.selectByPrimaryKey(id);
+        public  String getUserByPrimaryKey(@RequestParam("userid") String userid){
+           SysUser user = userService.selectByPrimaryKey(userid);
             System.out.println(user.toString());
             return user.toString();
         }
