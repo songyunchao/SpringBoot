85739bb950471db7c248eadfc8ec2c5ca08b6d85
fsun <385207720@qq.com>
2020-05-06 22:47:54 +0800

    11

--- config-controller/src/main/java/com/fsun/web/controller/PowerController.java
+++ config-controller/src/main/java/com/fsun/web/controller/PowerController.java
@@ -2,14 +2,11 @@ package com.fsun.web.controller;
 
 import java.util.List;
 
-import org.apache.shiro.authz.annotation.Logical;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
